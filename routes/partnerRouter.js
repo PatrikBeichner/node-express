d7842c14361f7dde41b40f0af625931699739d96
Patrik Beichner <patrik.beichner@gmail.com>
2021-01-30 11:25:24 -0500

    Partner & promotion routers

--- routes/partnerRouter.js
+++ routes/partnerRouter.js
@@ -1,46 +1,48 @@
-const express = require('express');
+const express = require("express");
 const partnerRouter = express.Router();
 
-partnerRouter.route('/')
-.all((req, res, next) => {
+partnerRouter
+  .route("/")
+  .all((req, res, next) => {
     res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
+    res.setHeader("Content-Type", "text/plain");
     next();
-})
-.get((req, res) => {
-    res.end('Will send all of the partners to you');
-})
-.post((req,res) => {
+  })
+  .get((req, res) => {
+    res.end("Will send all of the partners to you");
+  })
+  .post((req, res) => {
     res.end(`Will add the partner: ${req.body.name} with description: ${req.body.description}`);
-})
-.put((req, res) => {
+  })
+  .put((req, res) => {
     res.statusCode = 403;
-    res.end('PUT operation not supported on /campsites');
-})
-.delete((req, res) => {
-    res.end('Deleting all partners');
-});
+    res.end("PUT operation not supported on /campsites");
+  })
+  .delete((req, res) => {
+    res.end("Deleting all partners");
+  });
 
-partnerRouter.route('/:partnerId')
-.all((req, res, next) => {
+partnerRouter
+  .route("/:partnerId")
+  .all((req, res, next) => {
     res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
+    res.setHeader("Content-Type", "text/plain");
     next();
-})
-.get((req, res) => {
+  })
+  .get((req, res) => {
     res.end(`Will send details of the partner: ${req.params.partnerId} to you`);
-})
-.post((req, res) => {
+  })
+  .post((req, res) => {
     res.statusCode = 403;
     res.end(`POST operation not supported on /partners/${req.params.partnerId}`);
-})
-.put((req, res) => {
+  })
+  .put((req, res) => {
     res.write(`Updating the partner: ${req.params.partnerId}\n`);
     res.end(`Will update the partner: ${req.body.name}
             with description: ${req.body.description}`);
-})
-.delete((req, res) => {
+  })
+  .delete((req, res) => {
     res.end(`Deleting partner: ${req.params.partnerId}`);
-});
+  });
 
 module.exports = partnerRouter;
